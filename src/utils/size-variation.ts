a26f37d796a1e9cb9d7925ec4fbbd71d93c1645b
Andreas Herd <mendrik76@gmail.com>
2023-03-25 13:35:27 +0200

    remove bad interface

--- src/utils/size-variation.ts
+++ src/utils/size-variation.ts
@@ -2,11 +2,6 @@ import { Dimension } from '../types'
 import { average } from './average'
 import { normalize } from './normalize'
 
-interface ImageSize {
-  width: number
-  height: number
-}
-
 export const sizeVariation = (rects: Dimension[]): number => {
   const avgWidth = average(rects.map(r => r.width))
   const avgHeight = average(rects.map(r => r.height))
