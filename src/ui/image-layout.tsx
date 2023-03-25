132b97815f652093c369ce98fd0a31bfa6745167
Andreas Herd <mendrik76@gmail.com>
2023-03-26 00:21:56 +0200

    remove memo

--- src/ui/image-layout.tsx
+++ src/ui/image-layout.tsx
@@ -3,15 +3,13 @@ import { Picture } from '../types'
 import { PictureListItem } from './picture-list-item'
 import { useImageList } from '../hooks/useImageList'
 import { useCalculate } from '../hooks/useCalculate'
-import { memo } from 'react'
-import { F } from 'ramda'
 
 type OwnProps = {
   images: Picture[]
 }
 
 // eslint-disable-next-line react/prop-types
-export const ImageLayout = memo<OwnProps>(({ images: initialImages }) => {
+export const ImageLayout = ({ images: initialImages }: OwnProps): JSX.Element => {
   const dimension = useWindowDimension()
   const images = useImageList(initialImages)
   const { status, error, result } = useCalculate(images, dimension)
@@ -27,7 +25,7 @@ export const ImageLayout = memo<OwnProps>(({ images: initialImages }) => {
       ))}
     </ul>
   )
-}, F)
+}
 
 // eslint-disable-next-line functional/immutable-data
 ImageLayout.displayName = 'ImageLayout'
