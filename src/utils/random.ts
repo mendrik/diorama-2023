bf62f0012352710e631c4b87d508ee1c75110397
Andreas Herd <mendrik76@gmail.com>
2023-03-27 21:27:51 +0300

    fix size scoring

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,2 +1,2 @@
 export const randomInt = (min: number, max: number): number =>
-  Math.floor(Math.random() * (max - min + 1)) + min
+  ((Math.random() * (max + 1)) | 0) + min
--- src/utils/size-variation.ts
+++ src/utils/size-variation.ts
@@ -1,16 +1,10 @@
 import { Dimension } from '../types'
 import { average } from './average'
-import { normalize } from './normalize'
 
 export const sizeVariation = (rects: Dimension[]): number => {
-  const avgWidth = average(rects.map(r => r.width))
-  const avgHeight = average(rects.map(r => r.height))
-
-  const rectScores = rects.map(rect => {
-    const widthCV = rect.width / avgWidth
-    const heightCV = rect.height / avgHeight
-    return (widthCV + heightCV) / 2
-  })
-
-  return average(normalize(rectScores))
+  const maxWidth = Math.max(...rects.map(r => r.width))
+  const maxHeight = Math.max(...rects.map(r => r.height))
+  const minWidth = Math.min(...rects.map(r => r.width))
+  const minHeight = Math.min(...rects.map(r => r.height))
+  return average([minWidth / maxWidth, minHeight / maxHeight])
 }
