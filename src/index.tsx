8f3263672f2a1f9dd3466bbcd5bc88eec554ce1f
Andreas Herd <mendrik76@gmail.com>
2023-12-12 21:58:47 +0200

    Update index.tsx
    typo

--- src/index.tsx
+++ src/index.tsx
@@ -20,5 +20,5 @@ Maybe.fromNullable(document.getElementById('root'))
           </StrictMode>
         )
       })
-      .catch(() => alert('Failed to load imagges'))
+      .catch(() => alert('Failed to load images'))
   )
