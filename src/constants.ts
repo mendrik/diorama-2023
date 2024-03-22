f348abfcb61ce78bc1d9d360285220c34e1f3a04
Andreas Herd <andreas.herd@mindmine.fi>
2024-03-22 12:28:52 +0200

    tweak layout

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { range } from 'ramda'
 
 export const images = range(1, 18).map(n => `./images/${n}.jpg`)
 export const maxComputationTime = 600 as Milliseconds
-export const sizeHomogeneity = .5 // the higher the value, the more pictures will approach the same size
+export const sizeHomogeneity = .8 // the higher the value, the more pictures will approach the same size
 export const aspectRatioThreshold = 0.98 // minimum requirement for aspect ratio match 0-1
 export const iconSize = 20
 export const initialImageAmount = 8
