c62b7796d1e4d2c6efc7750b1b76d6b0c881cb29
Andreas Herd <andreas.herd@mindmine.fi>
2024-03-24 16:18:43 +0200

    inline composition

--- src/layout/to-random-tree.ts
+++ src/layout/to-random-tree.ts
@@ -22,32 +22,28 @@ export function* generateTreeCompositions(pictures: Picture[]): Generator<Rect> 
   }
 }
 
-const combine = (first: Rect, second: Rect): Composition =>
-  new Composition(Boolean(randomInt(0, 1)), first, second)
-
 const toRandomTree = (compositions: Rect[]): Composition => {
   const max = compositions.length - 1
   if (max === 0) {
     return compositions[0] as Composition
   }
   if (max === 1) {
-    return combine(compositions[0], compositions[1])
+    return new Composition(randomInt(0, 1) === 0, compositions[0], compositions[1])
   }
   const index1 = randomInt(0, max)
   // eslint-disable-next-line no-var
   let index2
   do {
     index2 = randomInt(0, max)
-  } while(index1 === index2)
-  const merged = combine(compositions[index1], compositions[index2])
+  } while (index1 === index2)
+  const merged = new Composition(randomInt(0, 1) === 0, compositions[index1], compositions[index2])
 
   return toRandomTree(compositions.with(index1, merged).toSpliced(index2, 1))
 }
 
 // eslint-disable-next-line no-restricted-syntax
 export function* toRandomTreeGenerator(compositions: Picture[]): Generator<Rect> {
-  while(true) {
+  while (true) {
     yield toRandomTree(compositions)
   }
 }
-
