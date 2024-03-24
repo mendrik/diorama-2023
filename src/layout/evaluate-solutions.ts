7342a75e13eea203f17686c53b3cebfc14feaa9d
Andreas Herd <andreas.herd@mindmine.fi>
2024-03-24 12:22:18 +0200

    switch log to debug

--- src/layout/evaluate-solutions.ts
+++ src/layout/evaluate-solutions.ts
@@ -2,13 +2,13 @@
 import '../types/ramda'
 
 import type { PositionedPicture, NonEmptyArray, Solution } from '../types/types'
-import { evolve, last, pipe, prop, sortBy, takeLast } from 'ramda'
+import { clamp, evolve, last, pipe, prop, sortBy, takeLast } from 'ramda'
 import { aspectRatioAndSize } from '../constants'
 
 export const evaluateSolutions = (results: NonEmptyArray<Solution>): Solution => {
   const sorted = pipe(
     sortBy(aspectRatioAndSize),
-    takeLast(20 - results[0].pictures.length),
+    takeLast(clamp(5, 30, 30 - results[0].pictures.length)),
     sortBy(prop('score'))
   )(results)
   return evolve({ pictures: sortBy<PositionedPicture>(prop('url')) }, last(sorted) as Solution)
