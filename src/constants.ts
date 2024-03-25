745d1bace045db03385d1934cd758d5c49f661ad
Andreas Herd <andreas.herd@mindmine.fi>
2024-03-25 22:37:25 +0200

    update lib

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import type { Milliseconds } from './types/types'
 export const maxComputationTime = 400 as Milliseconds
 export const randomizeThreshold = 10 // if amount of pics is bigger we have too many permutations to iterate through, switch to random strategy
 export const iconSize = 30
-export const initialImageAmount = 6
+export const initialImageAmount = 7
 export const discardBadRatio = 0.8
 
 export enum ImageSet {
@@ -15,5 +15,5 @@ export enum ImageSet {
 export const maxImages = {
   [ImageSet.animals]: 18,
   [ImageSet.family]: 14,
-  [ImageSet.art]: 12
+  [ImageSet.art]: 13
 }
